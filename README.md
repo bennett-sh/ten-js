61d94dd84b4dc8a4dec098e736a54460d5bda18f
Bennett <110846042+bennett-sh@users.noreply.github.com>
2022-10-04 01:47:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TenJS
+##### A library for getting all numbers from zero to ten.
+
+## How to use
+1. Install
+```$ npm install tenjs```
+2. Import
+```const { ONE, TWO, ... } = require('tenjs');```
+3. Profit.
+
+## Motivation
+My motivation behind this project was that I always thought typing out all numbers was really annoying and I could do it in a better way so I created this package.
---
+++ lib.test.js
@@ -0,0 +1,46 @@
+
+const numbers = require('./lib');
+
+test('get the number 0', () => {
+  expect(numbers.ZERO).toBe(0)
+})
+
+test('get the number 1', () => {
+  expect(numbers.ONE).toBe(1)
+})
+
+test('get the number 2', () => {
+  expect(numbers.TWO).toBe(2)
+})
+
+test('get the number 3', () => {
+  expect(numbers.THREE).toBe(3)
+})
+
+test('get the number 4', () => {
+  expect(numbers.FOUR).toBe(4)
+})
+
+test('get the number 5', () => {
+  expect(numbers.FIVE).toBe(5)
+})
+
+test('get the number 6', () => {
+  expect(numbers.SIX).toBe(6)
+})
+
+test('get the number 7', () => {
+  expect(numbers.SEVEN).toBe(7)
+})
+
+test('get the number 8', () => {
+  expect(numbers.EIGHT).toBe(8)
+})
+
+test('get the number 9', () => {
+  expect(numbers.NINE).toBe(9)
+})
+
+test('get the number 10', () => {
+  expect(numbers.TEN).toBe(10)
+})
